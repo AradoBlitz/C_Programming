f52d40f4484d2d8f06c27ab1dd839c76291d12f9
vilidmi <vilidmi@vilidmi>
2017-04-15 15:14:21 +0300

    Created new demonstration project.

---
+++ encapsulation.in.c/src/encapsulation.in.c.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : c.c
+ Author      : Dmitry Vilyuzhanin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Data encapsulation in C language!!!"); /* prints !!!Data encapsulation in C language!!! */
+	return EXIT_SUCCESS;
+}
